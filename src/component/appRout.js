bf5206423f64c2d177ca498dbc8663030d1ee4c1
ibrohim74 <xibroxim11@gmail.com>
2023-08-22 16:13:26 +0500

    test

--- src/component/appRout.js
+++ src/component/appRout.js
@@ -3,7 +3,7 @@ import { Routes, Route,Navigate} from "react-router-dom";
 import {adminDash, publicRoute} from "../routs";
 import {observer} from "mobx-react-lite";
 import UserPageContact from "../pages/userPageContact";
-import Sidebar from "../pages/admin/sidebar";
+
 
 const AppRout = observer(() => {
         const local = localStorage.getItem('token');
@@ -13,7 +13,7 @@ const AppRout = observer(() => {
                 {
                     publicRoute.map(({path, Component}) => <Route key={path} path={path} element={Component} exact/>)
                 }
-                { local && adminDash.map(({path, Component}) => <Route key={path} path={path + "/*"} element={Component}/>)}
+                { local && adminDash.map(({path, Component}) => <Route key={path} path={`${path}/*`} element={Component} exact/>)}
 
 
                     <Route  exact path={`/:username`} element={<UserPageContact/>} />
